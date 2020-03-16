4b148321e9e66e4762d9bc716baf5febffb55e20
Vitaly Pobokin <pbk.vitaliy@gmail.com>
2020-03-17 00:53:17 +0300

    Extended lexer.NextToken function to support more TokenType's

--- token/token.go
+++ token/token.go
@@ -13,7 +13,7 @@ const (
 
 	// Identifiers + literals
 	IDENT = "IDENT" // add, foobar, x, y, ...
-	INT   = "INT" // 1343456
+	INT   = "INT"   // 1343456
 
 	// Operators
 	ASSIGN = "="
@@ -31,3 +31,15 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
